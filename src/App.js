829c0af2ff211f875aa3f26d0420886ce94f396c
git init <butkepavan55@gmail.com>
2022-02-13 16:25:06 +0530

    columns added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-import Grid from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 import MaterialTable from 'material-table'
+import { Avatar } from '@material-ui/core';
 
 
 
 
 
 function App() {
+    var columns = [
+    {title: "id", field: "id"},
+    {title: "Avatar", field: "avatar",  render: rowData => <Avatar maxInitials={1} size={40} round={true} src={rowData === undefined ? " " : rowData.avatar} />   },
+    {title: "First name", field: "first_name"},
+    {title: "Last name", field: "last_name"},
+    {title: "Mail ID", field: "email"}
+  ]
+
   return (
     <div className="App">
        <Grid item xs={3} ></Grid>
