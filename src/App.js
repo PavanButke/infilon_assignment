45465305bc57cd7b4a46218a65d16de6e8605116
git init <butkepavan55@gmail.com>
2022-02-13 16:19:39 +0530

    Grid n Table added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core';
+import MaterialTable from 'material-table'
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-        <h1>Project Started</h1>
+       <Grid item xs={3} ></Grid>
+       <Grid item xs={8} >  
+        <MaterialTable 
+            title="Infilon Assignment"
+            columns={columns}
+            data={data}
+            icons={tableIcons}
+
+        
+        ></MaterialTable>
+
+       </Grid>
+       <Grid item xs={3}></Grid>
     </div>
   );
 }
